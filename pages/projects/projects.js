3ff6430744b97ce9fa2210473611ebee53e376f3
dimodc <damyceomag@gmail.com>
2021-03-06 13:21:33 +0200

    pages completed

---
+++ pages/projects/projects.js
@@ -0,0 +1,13 @@
+// function openFolder {
+//     let folders = document.getElementsByClassName('fa-folder');
+//     let links = document.getElementsByClassName('proj-link');
+
+//     for (i = 0; links.length < 4; i++) {
+//         links[i].onclick = function () {
+//             folders[i].classList.remove('fa-folder');
+//             folders[i].classList.add('fa-folder-open');
+//         }
+//     }
+// }
+
+// openFolder();
